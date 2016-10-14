b15d7a628b7959fae584143a6046c7866c5ac611
victor <victor@gerfaud.fr>
2016-10-14 08:33:04 +0200

    v2.3

--- TPangular_Step3/js/controllers/EventController-s3.1.js
+++ TPangular_Step3/js/controllers/EventController-s3.1.js
@@ -20,6 +20,18 @@ function eventCtrlFnc($scope,$log,factory){
 		presentation.slidArray.push(slid);
 		$scope.currentPresentation = presentation;
 
+	};
+
+	$scope.selectCurrentSlid=function(slide){
+		$scope.currentSlide = slide;
+		$scope.showme = true;
+	};
+
+	$scope.isSlidContentEmpty=function(slid){
+		if(slid.contentMap[1]== undefined){
+			return true;
+		}
+		return false
 	}
 	return "qsdk";
 };
